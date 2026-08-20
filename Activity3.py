e8d0288de3c8627a119d781fb108d66999c445b0
Gioxani <lontocgiancarlo@gmail.com>
2026-08-20 12:44:19 +0800

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,31 @@
+#variables
+#int
+age = 18
+birthYear = 2008
+fingers = 10
+toes = 10
+
+#float
+weight = 65.7
+height = 187.4
+temp = 35.4
+angle = 182.3
+
+#string
+name = "Gian"
+ring = "Silver"
+pen = "Black"
+vehicle = "motorcycle"
+
+#boolean
+isTall = True
+isAttractive = True
+isCool = True
+isRich = False
+
+print(f"my name is {name} and i am {age} years old i have {fingers} fingers and {toes} toes i am born at year {birthYear} my weight is {weight}kg my height is {height}cm the temperature today is {temp}degree celsius and the sunrays is pointing at angle {angle} i own a ring that is {ring} and my vehicle is {vehicle}.")
+
+print(f"Am I tall? {'Yes' if isTall else 'No'}")
+print(f"Am I attractive? {'Yes' if isAttractive else 'No'}")
+print(f"Am I cool? {'Yes' if isCool else 'No'}")
+print(f"Am I rich? {'Yes' if isRich else 'No'}")
