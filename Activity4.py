34a206f87469a44c3267df0585b49aaea743ca99
Gioxani <lontocgiancarlo@gmail.com>
2026-08-24 20:19:39 +0800

    Add files via upload

---
+++ Activity4.py
@@ -0,0 +1,11 @@
+#escape sequence
+# \n - new line
+# \t - tab space
+# \r - carriage return / deprecated (obsolete)
+# \b - back space
+# \\ - insert slash
+# \" -insert double quotation
+
+Z = "\t a dog jumped \rover a tall \rfence \"using \n its mighty\b feet\" and \\legs"
+print(Z)
+
---
+++ code_challenge1.py
@@ -0,0 +1,11 @@
+#escape sequence
+# \n - new line
+# \t - tab space
+# \r - carriage return / deprecated (obsolete)
+# \b - back space
+# \\ - insert slash
+# \" -insert double quotation
+
+House = "\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\t*\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\n\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\n\t\t\t*\t*\t\t\t\tHi, My Name is Gian Carlo M. Lontoc\t\t\t\t*\t*\t\t\t\n\t\t\t*\t*\t\t\t\t\t  FROM BSIT 1-B\t\t\t\t\t\t*\t*\t\t\t\n\t\t\t*\t*\t\t\t\t\t\t\t\t\t\t\t\t*\t*\t\t\t\n\t\t\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t\t\t\n\t\t\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t\t\t"
+
+print(House)
