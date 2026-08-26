a689ebb911b475d5919263bf927f101247d1bc51
Gioxani <lontocgiancarlo@gmail.com>
2026-08-26 21:59:53 +0800

    Add files via upload

---
+++ Activity5.py
@@ -0,0 +1,19 @@
+#operators activity
+
+a = 9
+b = 4
+sum = a + b
+diff = a - b
+do = a * b
+ha = a / b
+ek = a ** b
+ik = a // b
+ok = a % b
+
+print("The sum of a and b is",sum)
+print("The diffirence of a and b is",diff)
+print("a""*""b",do)
+print("a""\\""b",ha)
+print("a""**""b",ek)
+print("a""/""/""b",ik)
+print("a""%""b",ok)
