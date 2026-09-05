10a5f5865d046ea08199b9d1d4f118d387baa671
Gioxani <lontocgiancarlo@gmail.com>
2026-09-05 15:58:31 +0800

    Add files via upload

---
+++ Activity7.py
@@ -0,0 +1,42 @@
+total = 6
+
+a = eval(input("enter number:"))
+total +=a
+print("you currently have:", total)
+
+b = eval(input("enter number:"))
+total -=b
+print("you currently have:", total)
+
+c = eval(input("enter number:"))
+total *=c
+print("you currently have:", total)
+
+d = eval(input("enter number:"))
+total /=d
+print("you currently have:", total)
+
+e = eval(input("enter number:"))
+total +=e
+print("you currently have:", total)
+
+f = eval(input("enter number:"))
+total -=f
+print("you currently have:", total)
+
+g = eval(input("enter number:"))
+total /=g
+print("you currently have:", total)
+
+h = eval(input("enter number:"))
+total +=h
+print("you currently have:", total)
+
+i = eval(input("enter number:"))
+total +=i
+print("you currently have:", total)
+
+j = eval(input("enter number:"))
+total -=j
+print("you currently have:", total)
+
