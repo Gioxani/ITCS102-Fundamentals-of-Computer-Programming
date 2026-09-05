d344527330181c3870dc5ea560d710c989aa954f
Gioxani <lontocgiancarlo@gmail.com>
2026-09-05 16:03:39 +0800

    Add files via upload

---
+++ Activity8.py
@@ -0,0 +1,21 @@
+hobbies =""
+
+a = input("What are your hobbies: ")
+hobbies += a + ","
+
+a = input("What are your hobbies: ")
+hobbies += a + ","
+
+a = input("What are your hobbies: ")
+hobbies += a + ","
+
+a = input("What are your hobbies: ")
+hobbies += a + ","
+
+a = input("What are your hobbies: ")
+hobbies += a + ","
+
+a = input("What are your hobbies: ")
+hobbies += a +"."
+
+print("My hobbies are --> ",hobbies)
